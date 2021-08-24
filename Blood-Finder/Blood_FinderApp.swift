4b8f7bace10f8967de4391840133634a16ac9e0e
shahriarRahmanShaon <shaon463@gmail.com>
2021-08-24 10:16:47 +0600

    Initial Commit

---
+++ Blood-Finder/Blood_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blood_FinderApp.swift
+//  Blood-Finder
+//
+//  Created by sergio shaon on 24/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Blood_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
